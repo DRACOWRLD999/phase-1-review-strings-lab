69f4927a88a85050eb3453c176116c4d0679c47b
Ahmed <hmoodhmood004@gmail.com>
2024-02-17 19:37:01 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let currentUser=`ahmed sabah`
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase()
+
+let shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`
+
+
